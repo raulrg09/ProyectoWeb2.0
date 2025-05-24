57a8b7e43be6621ab5ce1d2c7fd4ecc31a90e7ac
raulrg09 <raulruiz8482@gmail.com>
2025-05-24 12:25:08 -0600

    Navbar Dropdown & Sección Hero

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './componentes/Navbar/Navbar'
+import Hero from './componentes/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
